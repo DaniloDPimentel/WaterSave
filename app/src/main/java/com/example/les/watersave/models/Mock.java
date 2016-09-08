a887a42bdbbd11ec3e8f5f975d4e7a4a888c49c5
Diego Adolfo <diegoado@gmail.com>
2016-09-07 23:14:16 -0300

    Fix statistics calculus

--- app/src/main/java/com/example/les/watersave/models/Mock.java
+++ app/src/main/java/com/example/les/watersave/models/Mock.java
@@ -43,7 +43,7 @@ public class Mock {
         medicoes.add(medicao);
     }
 
-    public double getConsumo() {
+    private double getConsumo() {
         Calendar date = Calendar.getInstance();
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
@@ -58,7 +58,7 @@ public class Mock {
 
         for (Medicao medicao: tempMedicoes) {
             if (dateFormat.format(medicao.getData()).compareTo(startDate) >= 0) {
-                double medicaoLevel = CAIXA_CAP / medicao.getNivel();
+                double medicaoLevel = CAIXA_CAP * medicao.getNivel() / 100;
                 if (medicaoLevel > refLevel)
                         consumo += medicaoLevel - refLevel;
 
@@ -74,12 +74,7 @@ public class Mock {
     }
 
     public double getEstimativa() {
-        Calendar date = Calendar.getInstance();
-
-        double meanDay = getMediaConsumoDiaria();
-        int currDay = date.get(Calendar.DAY_OF_MONTH);
-        int lastDay = date.getActualMaximum(Calendar.DAY_OF_MONTH);
-
-        return (CAIXA_CAP - (lastDay - currDay) * meanDay) / meanDay;
+        double currLevel = CAIXA_CAP * medicoes.get(0).getNivel() / 100;
+        return currLevel / getMediaConsumoDiaria();
     }
 }
